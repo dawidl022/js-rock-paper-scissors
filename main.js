f0be88fd22e50b60ab023bee81c14e86147225f9
dawidl022 <domdaw7@gmail.com>
2021-08-15 09:22:23 +0100

    Reorganise js code

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// GAMEPLAY LOGIC
+
 const options = ["Rock", "Paper", "Scissors"];
 const optionsLower = options.map(option => option.toLowerCase());
 
@@ -14,6 +16,39 @@ function computerPlay() {
   return Math.floor(Math.random() * 3);
 }
 
+
+function userPlay(button) {
+  const userInput = button.dataset.choice;
+  return optionsLower.indexOf(userInput);
+}
+
+
+function playRound(playerSelection, computerSelection) {
+  // rather than comparing strings in multiple if statements
+  // the indexes are compared to see which combination gives the player a win
+  const result = playerSelection - computerSelection;
+  let msg;
+  let score;
+
+  if (result === 1 || result === -2) {
+    msg = "You win! " + beatsMessage(playerSelection, computerSelection);
+    score = 1;
+
+  } else if (result === 0) {
+    msg = "It's a draw!";
+    score = 0;
+
+  } else {
+    msg = "You lose! " + beatsMessage(computerSelection, playerSelection);
+    score = -1;
+  }
+
+  return {"msg": msg, "score": score};
+}
+
+
+// DOM presentation and interaction
+
 const resultsArea = document.querySelector(".results");
 const gameOverSection = document.querySelector(".game-over");
 const overallResult = document.querySelector(".overall-result");
@@ -21,6 +56,7 @@ const userScoreDisplay = document.querySelector("#user-score");
 const cpuScoreDisplay = document.querySelector("#cpu-score");
 
 const scores = { user: 0, cpu: 0 };
+
 
 function updateScores() {
   userScoreDisplay.textContent = scores.user;
@@ -40,6 +76,7 @@ function updateScores() {
   }
 }
 
+
 function resetGame() {
   scores.user = 0;
   scores.cpu = 0;
@@ -56,14 +93,10 @@ const resetBtn = document.querySelector("#reset-btn");
 resetBtn.addEventListener("click", resetGame);
 
 
-function userPlay(button) {
-  const userInput = button.dataset.choice;
-  return optionsLower.indexOf(userInput);
-}
-
 function choiceClick() {
   playerBtns.forEach(btn => {
-    btn.removeEventListener("click", choiceClick)
+    // prevent button spamming
+    btn.removeEventListener("click", choiceClick);
   });
   
   const userOption = userPlay(this);
@@ -100,27 +133,3 @@ const playerBtns = document.querySelectorAll(".choice-btn");
 playerBtns.forEach(btn => {
   btn.addEventListener("click", choiceClick);
 });
-
-
-function playRound(playerSelection, computerSelection) {
-  // rather than comparing strings in multiple if statements
-  // the indexes are compared to see which combination gives the player a win
-  const result = playerSelection - computerSelection;
-  let msg;
-  let score;
-
-  if (result === 1 || result === -2) {
-    msg = "You win! " + beatsMessage(playerSelection, computerSelection);
-    score = 1;
-
-  } else if (result === 0) {
-    msg = "It's a draw!";
-    score = 0;
-
-  } else {
-    msg = "You lose! " + beatsMessage(computerSelection, playerSelection);
-    score = -1;
-  }
-
-  return {"msg": msg, "score": score};
-}
